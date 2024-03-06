1ec2b4eee94cade195813dc24439e5a1c1dd1392
rohan4420 <155799282+rohan4420@users.noreply.github.com>
2024-03-06 23:56:17 +0530

    Delete EMS-Backend directory

--- EMS-Backend/src/main/java/com/employe/repository/EmployeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.employe.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.employe.entity.Employe;
-
-public interface EmployeRepository extends JpaRepository<Employe, Long>{
-
-}
